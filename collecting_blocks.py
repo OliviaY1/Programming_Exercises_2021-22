dcecac3009ab978c099fc98892ae29a8596665fc
OTY <yanqianyanxi@outlook.com>
2021-11-25 10:17:36 -0800

    Update all the current work on Nov 25

--- collecting_blocks.py
+++ collecting_blocks.py
@@ -1,23 +1,76 @@
-# Pygame Boilerplate
+# Collecting Blocks Example
 # Author: Ubial
-# 2021 Version
 
 
+import random
 import pygame
 
 pygame.init()
 
-WHITE = (255, 255, 255)
-BLACK = (  0,   0,   0)
-RED   = (255,   0,   0)
-GREEN = (  0, 255,   0)
-BLUE  = (  0,   0, 255)
-BGCOLOUR = (100, 100, 255)
+WHITE =     (255, 255, 255)
+BLACK =     (  0,   0,   0)
+RED   =     (255,   0,   0)
+GREEN =     (  0, 255,   0)
+BLUE  =     (  0,   0, 255)
+ETON_BLUE = (135, 187, 162)
+RAD_RED =   (255,  56, 100)
+BLK_CHOCOLATE = (25, 17, 2)
+
+BGCOLOUR =  WHITE
 
 SCREEN_WIDTH  = 800
 SCREEN_HEIGHT = 600
 SCREEN_SIZE   = (SCREEN_WIDTH, SCREEN_HEIGHT)
-WINDOW_TITLE  = "<<Your Title Here>>"
+WINDOW_TITLE  = "Collecing Blocks"
+
+
+class Player(pygame.sprite.Sprite):
+    """Describes a player object
+    A subclass of pygame.sprite.Sprite
+
+    Attributes:
+        image: Surface that is the visual
+            representation of our Block
+        rect: numerical representation of
+            our Block [x, y, width, height]
+    """
+    def __init__(self) -> None:
+        # Call the superclass constructor
+        super().__init__()
+
+        # Create the image of the block
+        self.image = pygame.image.load("./images/smb_smallmario.png")
+
+        # Based on the image, create a Rect for the block
+        self.rect = self.image.get_rect()
+
+
+class Block(pygame.sprite.Sprite):
+    """Describes a block object
+    A subclass of pygame.sprite.Sprite
+
+    Attributes:
+        image: Surface that is the visual
+            representation of our Block
+        rect: numerical representation of
+            our Block [x, y, width, height]
+    """
+    def __init__(self, colour: tuple, width: int, height: int) -> None:
+        """
+        Arguments:
+        :param colour: 3-tuple (r, g, b)
+        :param width: width in pixels
+        :param height: height in pixels
+        """
+        # Call the superclass constructor
+        super().__init__()
+
+        # Create the image of the block
+        self.image = pygame.Surface([width, height])
+        self.image.fill(colour)
+
+        # Based on the image, create a Rect for the block
+        self.rect = self.image.get_rect()
 
 def main() -> None:
     """Driver of the Python script"""
@@ -28,8 +81,37 @@ def main() -> None:
     # Create some local variables that describe the environment
     done = False
     clock = pygame.time.Clock()
+    num_blocks = 100
+    score = 0
+
+    pygame.mouse.set_visible(False)
+
+    # Create groups to hold Sprites
+    all_sprites = pygame.sprite.Group()
+    block_sprites = pygame.sprite.Group()
+
+    # Create all the block sprites and add to block_sprites
+    for i in range(num_blocks):
+        # Create a block (set its parameters)
+        block = Block(BLACK, 20, 15)
+
+        # Set a random location for the block inside the screen
+        block.rect.x = random.randrange(SCREEN_WIDTH - block.rect.width)
+        block.rect.y = random.randrange(SCREEN_HEIGHT - block.rect.height)
+
+        # Add the block to the block_sprites Group
+        # Add the block to the all_sprites Group
+        block_sprites.add(block)
+        all_sprites.add(block)
+
     # Create the Player block
-    player = Block()
+    player = Player()
+    # Add the Player to all_sprites group
+    all_sprites.add(player)
+
+    pygame.mouse.set_visible(False)
+
+
     # ----------- MAIN LOOP
     while not done:
         # ----------- EVENT LISTENER
@@ -39,10 +121,21 @@ def main() -> None:
 
         # ----------- CHANGE ENVIRONMENT
         # Process player movement based on mouse pos
+
         mouse_pos = pygame.mouse.get_pos()
-        player.rect = mouse_pos
+        player.rect.x, player.rect.y = mouse_pos
+
+        # Check all collisions between player and the blocks
+        blocks_collided = pygame.sprite.spritecollide(player, block_sprites, True)
+
+        for block in blocks_collided:
+            score += 1
+            print(f"Score: {score}")
         # ----------- DRAW THE ENVIRONMENT
         screen.fill(BGCOLOUR)      # fill with bgcolor
+
+        # Draw all sprites
+        all_sprites.draw(screen)
 
         # Update the screen
         pygame.display.flip()
--- snowscape.py
+++ snowscape.py
@@ -1,8 +1,8 @@
-# Pygame SNOWSCAPE
-# Author: OY
-# 2021 Version
-import random
+# Relaxing Snowscape
+# Author: Ubial
 
+
+import random
 import pygame
 
 pygame.init()
@@ -12,26 +12,44 @@ BLACK = (  0,   0,   0)
 RED   = (255,   0,   0)
 GREEN = (  0, 255,   0)
 BLUE  = (  0,   0, 255)
-BGCOLOUR = (100, 100, 255)
+BGCOLOUR = BLACK
 
-SCREEN_WIDTH  = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH  = 1024
+SCREEN_HEIGHT = 768
 SCREEN_SIZE   = (SCREEN_WIDTH, SCREEN_HEIGHT)
-WINDOW_TITLE  = "OY's Snowscape"
+WINDOW_TITLE  = "Relaxing Snowscape"
 
-class Snow:
+
+class Snowflake:
+    """Snowflake on the screen
+
+    Attributes:
+        size: radius of snowflake in px
+        coords: {x: int, y:int}
+        y_vel: falling velocity in px/sec
+        colour: (r, g, b)
+    """
     def __init__(self):
-        self.radius = 5
-        speed = random.randint(1,5)
-        self.fall_speed = speed
-        x = random.randint(1, 800)
-        self.x = x
-        self.y = SCREEN_HEIGHT
-    def update(self)->None:
-        self.y += self.fall_speed
-        if self.y > self.radius + SCREEN_HEIGHT:
-            return None
+        self.size = 2
+        # randomly place the snow on the screen
+        self.coords = [
+            random.randrange(0, SCREEN_WIDTH),
+            random.randrange(0, SCREEN_HEIGHT)
+        ]
+        self.y_vel = 4
+        self.colour = WHITE
 
+    def update(self):
+        """Update the location of the snow"""
+        # Changes the y portion of the coords
+        self.coords[1] += self.y_vel
+
+        # Reset position of snowflake if it reaches bottom
+        if self.coords[1] > SCREEN_HEIGHT:
+            self.coords = [
+                random.randrange(0, SCREEN_WIDTH),
+                random.randrange(-25, 0)
+            ]
 
 
 def main() -> None:
@@ -43,21 +61,24 @@ def main() -> None:
     # Create some local variables that describe the environment
     done = False
     clock = pygame.time.Clock()
-    num_snowflakes = 230
+    num_snowflakes = 250
     snowflakes = []
-    for i in range(num_snowflakes - 150):
-        close_snowflake = Snow()
+    # Create snowflakes in foreground
+    for i in range(num_snowflakes-150):
+        close_snowflake = Snowflake()
         close_snowflake.size = random.choice([4, 5, 6])
         close_snowflake.y_vel = random.choice([1, 2])
         snowflakes.append(close_snowflake)
-    for i in range(num_snowflakes - 100):
-        close_snowflake = Snow()
+    for i in range(num_snowflakes-100):
+        close_snowflake = Snowflake()
         close_snowflake.size = random.choice([3, 4])
         close_snowflake.y_vel = random.choice([2, 3])
         snowflakes.append(close_snowflake)
-        # Create snowflakes in background
+    # Create snowflakes in background
     for i in range(num_snowflakes):
-        snowflakes.append(Snow())
+        snowflakes.append(Snowflake())
+
+
     # ----------- MAIN LOOP
     while not done:
         # ----------- EVENT LISTENER
@@ -68,13 +89,16 @@ def main() -> None:
         # ----------- CHANGE ENVIRONMENT
         for snow in snowflakes:
             snow.update()
+
         # ----------- DRAW THE ENVIRONMENT
         screen.fill(BGCOLOUR)      # fill with bgcolor
-        # Draw the snow
 
+        pygame.draw.rect(screen, (150, 150, 255), [0, 500, SCREEN_WIDTH, 300])
 
+        # draw the snowflakes
         for snow in snowflakes:
-            pygame.draw.circle(screen, WHITE, (snow.x, snow.y), snow.radius)
+            pygame.draw.circle(screen, snow.colour, snow.coords, snow.size)
+
         # Update the screen
         pygame.display.flip()
 
